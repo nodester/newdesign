456c6675474fa90779090da6c9814b4ece225cb7
Chris Matthieu <chris@matthieu.us>
2011-12-13 18:09:14 -0700

    added express to server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (newdesign) is running..');
-}).listen(12952);
+var express = require("express"),
+    app = express.createServer();
+app.use(express.static(__dirname));
+app.listen(12952);
